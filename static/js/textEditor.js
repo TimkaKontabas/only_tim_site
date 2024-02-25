c6f385d29cc05711c0e215ac003580286a1e7f9c
TimkaKontabas <kontorvaevtimur@gmail.com>
2024-02-25 14:58:24 +0500

    upd text редактор,add page написания письма

--- static/js/textEditor.js
+++ static/js/textEditor.js
@@ -1,26 +1,72 @@
-const mainTextEditor = document.getElementById('mainTextEditor');
-const boldButton = document.getElementById('boldButton');
-const italicButton = document.getElementById('italicButton');
-const underlineButton = document.getElementById('underlineButton');
-const leftButton = document.getElementById('leftButton');
-const centerButton = document.getElementById('centerButton');
-const rightButton = document.getElementById('rightButton');
+const toolPanel = document.getElementById('TextEditorSarbToolPanel');
 
-const undoButton = document.getElementById('undoButton');
-const redoButton = document.getElementById('redoButton');
+let copy_text = '';
 
-const copyButton = document.getElementById('copyButton');
-const cutButton = document.getElementById('cutButton');
-const pasteButton = document.getElementById('pasteButton');
 
-const saveButton = document.getElementById('saveButton');
-const loadButton = document.getElementById('loadButton');
-const clearButton = document.getElementById('clearButton');
+function addButtonsToToolPanel() {
+    const buttonData = [
+        { id: 'boldButton', text: 'Жирным', img: 'bold.svg' },
+        { id: 'italicButton', text: 'Курсивом', img: 'italic.svg' },
+        { id: 'underlineButton', text: 'Подчеркнутым', img: 'underline.svg' },
+        { id: 'leftButton', text: 'По левому краю', img: 'align-left.svg' },
+        { id: 'centerButton', text: 'По центру', img: 'align-center.svg' },
+        { id: 'rightButton', text: 'По правому краю', img: 'align-right.svg' },
+        { id: 'justifyButton', text: 'По ширине', img: 'align-justify.svg' },
+        { id: 'numberedListButton', text: 'Нумерованный список', img: 'numbered-list.png' },
+        { id: 'bulletedListButton', text: 'Маркированный список', img: 'bulleted-list.png' },
+        { id: 'undoButton', text: 'Отменить', img: 'undo.svg' },
+        { id: 'redoButton', text: 'Вернуть', img: 'redo.svg' },
+        { id: 'copyButton', text: 'Копировать', img: 'copy.svg' },
+        { id: 'cutButton', text: 'Вырезать', img: 'cut.svg' },
+        { id: 'pasteButton', text: 'Вставить', img: 'paste.svg' },
+        { id: 'saveButton', text: 'Сохранить', img: 'save.svg' },
+        { id: 'loadButton', text: 'Загрузить', img: 'load.svg' },
+        { id: 'clearButton', text: 'Очистить', img: 'clear.svg' },
+        { id: 'addLinkButton', text: 'Добавить ссылку', img: 'link.svg' },
+        { id: 'addImageButton', text: 'Добавить картинку', img: 'image.svg' },
+    ];
 
-const addLinkButton = document.getElementById('addLinkButton');
-const addImageButton = document.getElementById('addImageButton');
+    buttonData.forEach(button => {
+        const newButton = document.createElement('button');
+        newButton.id = button.id;
+        newButton.className = 'TextEditorSarbToolButton';
+        if (button.img === undefined) {
+            newButton.textContent = button.text;
+        } else {
+            newButton.style.backgroundImage = `url(../static/img/textEditor/text-${button.img})`;
+        }
+        toolPanel.appendChild(newButton);
+    });
 
-document.execCommand('aShowDefaultUI', false, false);
+    addHandlersToButtons();
+}
+
+function addHandlersToButtons() {
+    const mainTextEditor = document.getElementById('TextEditorSarbTextArea');
+    const boldButton = document.getElementById('boldButton');
+    const italicButton = document.getElementById('italicButton');
+    const underlineButton = document.getElementById('underlineButton');
+    const leftButton = document.getElementById('leftButton');
+    const centerButton = document.getElementById('centerButton');
+    const rightButton = document.getElementById('rightButton');
+    const justifyButton = document.getElementById('justifyButton');
+    const numberedListButton = document.getElementById('numberedListButton');
+    const bulletedListButton = document.getElementById('bulletedListButton');
+
+    const undoButton = document.getElementById('undoButton');
+    const redoButton = document.getElementById('redoButton');
+
+    const copyButton = document.getElementById('copyButton');
+    const cutButton = document.getElementById('cutButton');
+    const pasteButton = document.getElementById('pasteButton');
+
+    const saveButton = document.getElementById('saveButton');
+    const loadButton = document.getElementById('loadButton');
+    const clearButton = document.getElementById('clearButton');
+
+    const addLinkButton = document.getElementById('addLinkButton');
+    const addImageButton = document.getElementById('addImageButton');
+
 
 mainTextEditor.addEventListener('input', () => {
     document.execCommand('defaultParagraphSeparator', false, 'div');
@@ -45,6 +91,15 @@ centerButton.addEventListener('click', () => {
 rightButton.addEventListener('click', () => {
     document.execCommand('justifyRight', false, null);
 });
+justifyButton.addEventListener('click', () => {
+    document.execCommand('justifyFull', false, null);
+});
+numberedListButton.addEventListener('click', () => {
+    document.execCommand('insertOrderedList', false, null);
+});
+bulletedListButton.addEventListener('click', () => {
+    document.execCommand('insertUnorderedList', false, null);
+});
 
 undoButton.addEventListener('click', () => {
     document.execCommand('undo', false, null);
@@ -53,14 +108,42 @@ redoButton.addEventListener('click', () => {
     document.execCommand('redo', false, null);
 });
 
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+async function copyContent(text) {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log('Content copied to clipboard');
+      /* Resolved - text copied to clipboard successfully */
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+      /* Rejected - text failed to copy to the clipboard */
+    }
+  }
+
+
 copyButton.addEventListener('click', () => {
-    document.execCommand('copy', false, null);
+    let text = getSelectionText();
+    copyContent(text);
 });
 cutButton.addEventListener('click', () => {
+    let text = getSelectionText();
     document.execCommand('cut', false, null);
+    copyContent(text);
 });
 pasteButton.addEventListener('click', () => {
-    document.execCommand('paste', false, null);
+    let text = navigator.clipboard.readText().then(text => {
+        console.log(text);
+        document.execCommand('insertText', false, text);
+    });
 });
 
 saveButton.addEventListener('click', () => {
@@ -109,5 +192,15 @@ clearButton.addEventListener('click', () => {
         mainTextEditor.removeChild(child);
     })
 });
+
+}
+
+
+
+
+addButtonsToToolPanel();
+
+
+document.execCommand('aShowDefaultUI', false, false);
 
 
